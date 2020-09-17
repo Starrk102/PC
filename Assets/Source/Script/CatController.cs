2fa1fe71dc0595bc2664713ac0cf2a71d670f73d
Starrk102 <Oriharaizzy@gmail.com>
2020-09-18 00:13:09 +0500

    complite test

--- Assets/Source/Script/CatController.cs
+++ Assets/Source/Script/CatController.cs
@@ -6,39 +6,143 @@ public class CatController : MonoBehaviour, IMood
 {
     [SerializeField]
     private UniversalScriptableObject CatScriptableObject;
-    [Range(0, 10f)] private float mood;
+    [Range(0, 2f)] private float mood;
+    private Animator animator;
+    private string text;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         mood = CatScriptableObject.Mood;
+        animator = GetComponent<Animator>();
+        if(mood == 0)
+        {
+            animator.Play("Idle_1");
+            text = "Ваша кошка грустит";
+        }
+        else if(mood == 1)
+        {
+            animator.Play("Idle_2");
+            text = "Вашей кошке хорошо";
+        }        
+        else if(mood == 2)
+        {
+            animator.Play("Idle_3");
+            text = "Ваша кошка счастлива";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayToCat()
     {
-        
+        if(mood == 0)
+        {
+            animator.Play("Play_1");
+            var damage = 0;
+            ApplayDamage(damage);
+            text = "Кошка сидит на месте";
+        }
+        else if(mood == 1)
+        {
+            animator.Play("Play_2");
+            var damage = 1;
+            ApplayDamage(damage);
+            text = "Кошка медленно бегает за мячиком";
+        }
+        else if(mood == 2)
+        {
+            animator.Play("Play_3");
+            var damage = 0;
+            ApplayDamage(damage);
+            text = "кошка носится, как угорелая";
+        }
     }
 
-    public void PlayToCat_1()
+    public void PetToCat()
     {
-        //Animator.Play();
-        var damage = 0;
-        ApplayDamage(damage);
+        if (mood == 0)
+        {
+            animator.Play("Pet_1");
+            var damage = 0;
+            ApplayDamage(damage);
+            text = "Кошка царапается";
+        }
+        else if (mood == 1)
+        {
+            animator.Play("Pet_2");
+            var damage = 1;
+            ApplayDamage(damage);
+            text = "Кошка мурлычет";
+        }
+        else if (mood == 2)
+        {
+            animator.Play("Pet_3");
+            var damage = 0;
+            ApplayDamage(damage);
+            text = "Кошка мурлычит и виляет хвостом";
+        }
     }
 
-    public void PlayToCat_2()
+    public void FoodToCat()
     {
-
+        if (mood == 0)
+        {
+            animator.Play("Food_1");
+            var damage = 1;
+            ApplayDamage(damage);
+            text = "Кошка все съедает, но если подойти - поцарапает";
+        }
+        else if (mood == 1)
+        {
+            animator.Play("Food_2");
+            var damage = 1;
+            ApplayDamage(damage);
+            text = "Кошка быстро все съедает";
+        }
+        else if (mood == 2)
+        {
+            animator.Play("Food_2");
+            var damage = 0;
+            ApplayDamage(damage);
+            text = "Кошка быстро все съедает";
+        }
     }
 
-    public void PlayToCat_3()
+    public void KickToCat()
     {
-
+        if (mood == 0)
+        {
+            animator.Play("Kick_1");
+            var damage = 0;
+            ApplayDamage(damage);
+            text = "Кошка прыгает и кусает за правое ухо";
+        }
+        else if (mood == 1)
+        {
+            animator.Play("Kick_2");
+            var damage = -1;
+            ApplayDamage(damage);
+            text = "Кошка убегает на ковер и писает";
+        }
+        else if (mood == 2)
+        {
+            animator.Play("Kick_3");
+            var damage = -2;
+            ApplayDamage(damage);
+            text = "Кошка убегает в другую комнату";
+        }
     }
 
     public void ApplayDamage(float damage)
     {
         mood += damage;
+    }
+
+    public string GetText()
+    {
+        return text;
+    }
+
+    public float GetMood()
+    {
+        return mood;
     }
 }
---
+++ Assets/Source/Script/UIPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayer : MonoBehaviour
+{
+    private Image imageIndecatorMood;
+    private GameObject cat;
+    private float mood;
+    private Text descriptionCat;
+
+    private void Start()
+    {
+        imageIndecatorMood = gameObject.transform.GetChild(0).gameObject.transform.GetChild(4).gameObject.GetComponent<Image>();
+        cat = GameObject.FindGameObjectWithTag("Player");
+        descriptionCat = GameObject.FindGameObjectWithTag("UIText").gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>();
+        mood = cat.GetComponent<CatController>().GetMood();
+        UpdateText();
+    }
+
+    private void Update()
+    {
+        mood = cat.GetComponent<CatController>().GetMood();
+
+        if (mood == 0)
+        {
+            imageIndecatorMood.color = Color.red;
+        }
+        else if(mood == 1)
+        {
+            imageIndecatorMood.color = Color.yellow;
+        }
+        else if(mood == 2)
+        {
+            imageIndecatorMood.color = Color.green;
+        }
+    }
+
+    public void UpdateText()
+    {
+        descriptionCat.text = cat.GetComponent<CatController>().GetText();
+    }
+}
